d0cb91bae8231de1ea463d800c55363646fa44d7
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-10-02 13:04:41 -0300

    feat: add repository pattern

--- src/Interface/IProductRepository.cs
+++ src/Interface/IProductRepository.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.src.Dtos;
+using api.src.models;
 
 namespace api.src.Interface
 {
     public interface IProductRepository
     {
-        
+        Task<List<Product>> GetAll();
+        Task<Product?> GetById(int id);
+        Task<Product> Post(Product product);
+        Task<Product?> Put(int id, UpdateProductRequestDto productDto);
+        Task<Product?> Delete(int id);
     }
 }
